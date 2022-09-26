c06d0e0a13ae1d3d00e6473ea8d85a425da890c9
Muhammad Ali <reddragon33795@gmail.com>
2022-09-26 00:54:15 -0700

    Changed File name for output.css to index.css and merged all .js into index.js

--- index.js
+++ index.js
@@ -133,4 +133,85 @@ for (let i = 0; i < button.length; i++) {
         }
     };
     
-    
+// Nav Mobile Combining for Host
+let menue = document.getElementById('menue');
+let navHidden = document.getElementById('navHidden')
+//Adding Event listner to know when menue button is pressed
+menue.addEventListener('click', () => {
+    if (navHidden.classList.contains('hidden')) {
+        navHidden.classList.remove('hidden');
+    } else {
+        navHidden.classList.add('hidden');
+    }
+});
+
+//Bg Sound
+let bgSound1 = new Audio('./audio/bgSound1.mp3');
+let bgSound2 = new Audio('./audio/bgSound2.mp3');
+let mute = 1;
+
+//Mobile Nav Functionality
+    //Themes
+        let theme1 = document.getElementById('theme1');
+        let theme2 = document.getElementById('theme2');
+    //Background Music
+        let song1 = document.getElementById('songOn');
+        let song2 = document.getElementById('songOff');
+        //Using Event listner to know when buttons are clicked
+               song1.addEventListener('click', () => {
+                playbg();
+               });
+               song2.addEventListener('click', () => {
+                bgSound1.pause();
+                bgSound2.pause();
+               });
+    //Button Audio
+        let buttonSound1 = document.getElementById('audioOn');
+        let buttonSound2 = document.getElementById('audioOff');
+        //Using Event listner to know when buttons are clicked
+               buttonSound1.addEventListener('click', () => {
+                mute = 1;
+               });
+               buttonSound2.addEventListener('click', () => {
+                mute = 0;
+               });
+        
+    //Bg Audio
+        function playbg() {
+            if (bgSound1.paused == true && bgSound2.paused == true) {
+                bgSound1.play();
+            } else if (bgSound1.paused == true) {
+                bgSound2.play();
+            }    
+        };
+
+//Combining Theme.js for host
+//Getting all elements to be changed
+let menueTheme2 = document.getElementById('menue');
+let navTheme2 = document.getElementById('navHidden');
+let bodyTheme2 = document.querySelector('body');
+let calTheme2 = document.getElementById('callBox');
+let tableTheme2 = document.getElementById('tableId');
+
+//Getting Buttons
+let theme1Button = document.getElementById('theme1');
+let theme2Button = document.getElementById('theme2');
+
+//Changing To theme 1
+theme1Button.addEventListener('click', () => {
+    console.log('theme1');//Debug purpose
+    menueTheme2.classList.remove('menueCssTheme2');
+    navTheme2.classList.remove('dropMenueTheme2');
+    bodyTheme2.classList.remove('bodyTheme2');
+    calTheme2.classList.remove('calBoxTheme2');
+    tableTheme2.classList.remove('tableTheme2');
+});
+//Changing To theme 2
+theme2Button.addEventListener('click', () => {
+    console.log('theme2');//Debug purpose
+    menueTheme2.classList.add('menueCssTheme2');
+    navTheme2.classList.add('dropMenueTheme2');
+    bodyTheme2.classList.add('bodyTheme2');
+    calTheme2.classList.add('calBoxTheme2');
+    tableTheme2.classList.add('tableTheme2');
+});
